0606049eb01bfe48b8e1d37f08b227876b8c9a48
Aemitez <watcharapong_aem@hotmail.com>
2018-10-29 15:03:19 +0700

    Add files via upload
    update

--- simple Twain/Form1.Designer.cs
+++ simple Twain/Form1.Designer.cs
@@ -30,31 +30,34 @@
         {
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
-            this.twain321 = new Saraff.Twain.Twain32(this.components);
+            this.twain = new Saraff.Twain.Twain32(this.components);
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(122, 87);
+            this.button1.Location = new System.Drawing.Point(105, 49);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Scan Now";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // twain321
+            // twain
             // 
-            this.twain321.AppProductName = "Saraff.Twain.NET";
-            this.twain321.Parent = null;
+            this.twain.AppProductName = "Saraff.Twain.NET";
+            this.twain.Parent = null;
+            this.twain.AcquireCompleted += new System.EventHandler(this.twain_AcquireCompleted);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(284, 126);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scan ";
             this.ResumeLayout(false);
 
         }
@@ -62,7 +65,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private Saraff.Twain.Twain32 twain321;
+        private Saraff.Twain.Twain32 twain;
     }
 }
 
--- simple Twain/Form1.cs
+++ simple Twain/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Saraff.Twain;
 
 namespace simple_Twain
 {
@@ -15,6 +11,50 @@ namespace simple_Twain
         public Form1()
         {
             InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                twain.CloseDSM();
+                twain.CloseDataSource();
+                if (twain.SelectSource())
+                {
+                    twain.Language = Saraff.Twain.TwLanguage.ENGLISH;
+                    twain.Acquire();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.StackTrace, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+            
+
+        }
+
+        private void twain_AcquireCompleted(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < twain.ImageCount; i++)
+                {
+                    string filePath = $".\\Photo_{DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")}.tif";
+                    ImageCodecInfo image = ImageCodecInfo.GetImageEncoders().FirstOrDefault(x => x.MimeType == "image/tiff");
+                    Encoder myEncoder = Encoder.Compression;
+                    EncoderParameters encoderParameters = new EncoderParameters(1);
+                    EncoderParameter encoderParameter = new EncoderParameter(myEncoder, (long)EncoderValue.CompressionCCITT4);
+                    encoderParameters.Param[0] = encoderParameter;
+                    twain.GetImage(i).Save(filePath, image, encoderParameters);
+                    MessageBox.Show(this, "Complete file in your project", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.StackTrace, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+          
         }
     }
 }
